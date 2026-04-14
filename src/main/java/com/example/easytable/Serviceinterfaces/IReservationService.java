869e137f0a4df14de08d2c9dcb11bbb211b88116
RicardoSulcaRojas <u202316491@upc.edu.pe>
2026-04-14 01:54:29 -0500

    CRUD Reservation completo

---
+++ src/main/java/com/example/easytable/Serviceinterfaces/IReservationService.java
@@ -0,0 +1,16 @@
+package com.example.easytable.Serviceinterfaces;
+
+import com.example.easytable.Entities.Reservation;
+
+import java.util.List;
+
+public interface IReservationService {
+    public List<Reservation> list();
+    public void insert(Reservation reservation);
+    public void delete(int id);
+    public void update(Reservation reservation);
+    public Reservation listId(int id);
+
+    public List<Reservation> findByUserId(int userId);
+    public List<Reservation> findByRestaurantId(int restaurantId);
+}
