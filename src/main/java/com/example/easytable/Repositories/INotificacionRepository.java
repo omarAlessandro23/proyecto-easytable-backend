decc40f6aa917e5d77330aa4cd29fa3f20e2b1f0
Jean Pierre Ulffe <jeanpierre_8a@hotmail.com>
2026-04-29 16:13:31 -0500

    Limpieza de clases duplicadas y actualización de lógica de Category y Favorite

--- src/main/java/com/example/easytable/Repositories/INotificacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.easytable.Repositories;
-
-import com.example.easytable.Entities.Notificacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface INotificacionRepository extends JpaRepository<Notificacion, Integer> {
-}
